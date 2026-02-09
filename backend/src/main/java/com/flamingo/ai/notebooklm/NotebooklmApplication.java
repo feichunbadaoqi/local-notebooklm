56cf5ce79065cdeb757effe6ec5b59208e7647f1
liangchen <feichunbadaoqi@gmail.com>
2026-02-08 20:18:25 -0800

    git init

---
+++ backend/src/main/java/com/flamingo/ai/notebooklm/NotebooklmApplication.java
@@ -0,0 +1,13 @@
+package com.flamingo.ai.notebooklm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebooklmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebooklmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/flamingo/ai/notebooklm/NotebooklmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flamingo.ai.notebooklm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebooklmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
